f247d8120c0f4baa4380eeaa9d5f4449b413ad3d
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-12 21:20:58 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,11 @@ public class Main {
   public static void main(String[] args){
     RWOnly obj = new RWOnly();
 
-    // obj.variable = 10;
+    obj.variable = 10; 
+    // Error: 'name' has private access in RWOnly
+    
+    System.out.println(obj.variable); 
+    // Error: 'name' has private access in RWOnly
     
     obj.setter(100);
     
