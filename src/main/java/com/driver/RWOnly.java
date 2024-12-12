63833992575a71316e29e5085fcdb90c223446a6
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-12 21:15:57 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int variable;
+
+    public int setter(int num){
+        this.variable = num;
+    }
+    public int getter(){
+        return variable;
+    }
 }
