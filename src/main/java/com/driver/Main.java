113830e3e71d1886351f6fdd1ebe84ce708a29e7
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-12 21:15:55 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly obj = new RWOnly();
+
+    // obj.variable = 10;
+    
+    obj.setter(100);
+    
+    int result = obj.getter();
+    
+    System.out.println(result);
+  }
 }
