399593f18e0cf5d9d5ee30d45f7006c0b0f93a5b
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2024-12-12 21:31:09 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,10 +3,11 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    public String setName(String str){
-        this.name = str;
-    }
     public String getName(){
         return name;
+    }
+
+    public void setName(String str){
+        this.name = str;
     }
 }
